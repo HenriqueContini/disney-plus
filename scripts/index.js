12d92607da1a1b1047185178c9bf848d2f45ecf4
HC Dev <81761545+HenriqueContini@users.noreply.github.com>
2022-07-31 22:09:05 -0300

    Criação do menu lateral e refatoração do index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,49 @@
 const API_KEY = '58a978411da62e93d626c9b0ded64650';
 const API_LANGUAGE = 'pt-br';
-
-const LIST_MOVIES = ['tt12801262', 'tt4823776', 'tt5109280', 'tt7146812', 'tt2948372', 'tt3521164', 'tt2096673', 'tt2953050', 'tt2380307', 'tt8097030'];
 const BASE_URL_IMAGE = {
     original: 'https://image.tmdb.org/t/p/original',
     small: 'https://image.tmdb.org/t/p/w500'
 };
 
+const movies = [];
+const moviesElement = document.getElementById('movies');
+
 function getUrlMovie(movieId) {
     return `https://api.themoviedb.org/3/movie/${movieId}?language=${API_LANGUAGE}&api_key=${API_KEY}`;
 }
 
-function setMainMovie(movieId) {
-    fetch(getUrlMovie(movieId)).then(response => response.json()).then(data => {
-        const appImage = document.querySelector('.app__image img');
+function changeButtonMenu() {
+    const button = document.querySelector('.button__menu');
+    button.classList.toggle('active');
 
-        const title = document.querySelector('.feature__movie h1');
-        const description = document.querySelector('.feature__movie p');
-        const info = document.querySelector('.feature__movie span');
-        const rating = document.querySelector('.rating strong');
-    
-        const yearRelease = data.release_date.split('-')[0];
-    
-        title.innerHTML = data.title;
-        description.innerHTML = data.overview;
-        rating.innerHTML = data.vote_average;
-        info.innerHTML = `${yearRelease} - ${data.genres[0].name} - Movie`;
-    
-        const image = BASE_URL_IMAGE.original.concat(data.backdrop_path);
-        appImage.setAttribute('src', image)
-    })
+    const navigation = document.querySelector('.navigation');
+    navigation.classList.toggle('active');
+}
+
+function setMainMovie(movie) {
+    const appImage = document.querySelector('.app__image img');
+    const title = document.querySelector('.feature__movie h1');
+    const description = document.querySelector('.feature__movie p');
+    const info = document.querySelector('.feature__movie span');
+    const rating = document.querySelector('.rating strong');
+
+    title.innerHTML = movie.title;
+    description.innerHTML = movie.overview;
+    rating.innerHTML = movie.vote_average;
+    info.innerHTML = `${movie.release} - ${movie.genre} - Movie`;
+
+    appImage.setAttribute('src', movie.image.original);
+}
+
+function changeMainMovie(movieId) {
+    const movie = movies.find(movie => movie.id === movieId);
+    setMainMovie(movie);
+    changeButtonMenu();
 }
 
 function createButtonMovie(movieId) {
     const button = document.createElement('button');
-    button.setAttribute('onclick', `setMainMovie('${movieId}')`)
+    button.setAttribute('onclick', `changeMainMovie('${movieId}')`);
     button.innerHTML = '<img src="/assets/icon-play-button.png" alt="Play button" />';
     return button;
 }
@@ -53,29 +62,48 @@ function createImageMovie(movieImage, movieTitle) {
     return divImageMovie;
 }
 
-const moviesList = document.getElementById('movies__list');
 
-function createMovie(movieId) {
-    fetch(getUrlMovie(movieId)).then(response => response.json()).then(data => {
-        const movie = document.createElement('li');
-        movie.classList.add('movie');
+function addMovieInList(movie) {
+    const movieElement = document.createElement('li');
+    movieElement.classList.add('movie');
 
-        const genre = `<span>${data.genres[0].name}</span>`;
-        const title = `<strong>${data.title}</strong>`;
-        const image = BASE_URL_IMAGE.small.concat(data.backdrop_path);
+    const genre = `<span>${movie.genre}</span>`;
+    const title = `<strong>${movie.title}</strong>`;
 
-        movie.innerHTML = genre + title;
-        movie.appendChild(createButtonMovie(movieId));
-        movie.appendChild(createImageMovie(image, data.title));
+    movieElement.innerHTML = genre + title;
+    movieElement.appendChild(createButtonMovie(movie.id));
+    movieElement.appendChild(createImageMovie(movie.image.small, movie.title));
 
-        moviesList.appendChild(movie);
+    moviesElement.appendChild(movieElement);
+}
+
+function loadMovies() {
+    const LIST_MOVIES = ['tt12801262', 'tt4823776', 'tt7146812', 'tt2948372', 'tt3521164', 'tt2096673', 'tt5109280', 'tt2380307'];
+
+    LIST_MOVIES.map((movie, index) => {
+        fetch(getUrlMovie(movie)).then(response => response.json()).then(data => {
+            const movieData = {
+                id: movie,
+                title: data.title,
+                overview: data.overview,
+                vote_average: data.vote_average,
+                genre: data.genres[0].name,
+                release: data.release_date.split('-')[0],
+                image: {
+                    original: BASE_URL_IMAGE.original.concat(data.backdrop_path),
+                    small: BASE_URL_IMAGE.small.concat(data.backdrop_path)
+                }
+            }
+            
+            movies.push(movieData);
+
+            if(index === 0) {
+                setMainMovie(movieData);
+            }
+
+            addMovieInList(movieData)
+        })
     })
 }
 
-function loadListMovies() {
-    LIST_MOVIES.map(createMovie);
-}
-
-loadListMovies()
-// Script para inicializar os dados do filme principal
-setMainMovie(LIST_MOVIES[0])
+loadMovies();
